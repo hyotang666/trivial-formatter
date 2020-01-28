29c2b79af64bc194faed918ae159c570e73fdbc6
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-01-28 22:37:15 +0900

    Add: APPENDER

--- src/trivial-formatter.lisp
+++ src/trivial-formatter.lisp
@@ -141,6 +141,15 @@
     (with-open-file(*standard-output* pathname :direction :output :if-exists :supersede)
       (mapc #'print-as-code new))))
 
+(defun appender(component)
+  (let*((pathname
+          (asdf:component-pathname component))
+        (new
+          (uiop:while-collecting(acc)
+            (call-with-file-exp pathname #'acc))))
+    (with-open-file(*standard-output* pathname :direction :output :if-exists :append)
+      (mapc #'print-as-code new))))
+
 (defun debug-printer (component)
   (call-with-file-exp (asdf:component-pathname component)
                       #'print-as-code))
--- trivial-formatter.asd
+++ trivial-formatter.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "trivial-formatter"
   :version
-  "0.31.0"
+  "0.32.0"
   :depends-on
   (
    "read-as-string" ; Read S-Expression as string.
