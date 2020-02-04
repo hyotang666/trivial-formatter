983365bf5452af0c771d5582842020cd36cb0d81
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-02-04 14:29:46 +0900

    Add: Spec template for PARSE-LOOP-BODY

--- spec/trivial-formatter.lisp
+++ spec/trivial-formatter.lisp
@@ -1,5 +1,5 @@
 (defpackage :trivial-formatter.spec
-  (:import-from :trivial-formatter #:pprint-extended-loop)
+  (:import-from :trivial-formatter #:pprint-extended-loop #:parse-loop-body)
   (:use :cl :jingoh :trivial-formatter))
 (in-package :trivial-formatter.spec)
 (setup :trivial-formatter)
@@ -321,3 +321,25 @@
         ELSE
           DO (ERROR \"found a funny value in list ~S, value ~S~%\" LIST I)
       FINALLY (RETURN (VALUE FLOAT-NUMBERS OTHER-NUMBERS SYMBOL-LIST)))"
+
+(requirements-about PARSE-LOOP-BODY :doc-type function)
+
+;;;; Description:
+
+#+syntax
+(PARSE-LOOP-BODY body) ; => result
+
+;;;; Arguments and Values:
+
+; body := 
+
+; result := 
+
+;;;; Affected By:
+
+;;;; Side-Effects:
+
+;;;; Notes:
+
+;;;; Exceptional-Situations:
+
